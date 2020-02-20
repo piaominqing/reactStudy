b77f401686f7c1442fa34bf86a08fcf8af907044
LF90187 <piao_minqing@nhn-st.com>
2020-02-20 17:26:12 +0800

    update function component

--- first-app/src/component/FunctionComponent.js
+++ first-app/src/component/FunctionComponent.js
@@ -1,20 +1,27 @@
 import React, {useState, useEffect} from 'react'
 
 export default function FunctionComponent (){
-    const [date, setState] = useState(new Date())
-    // 第一个参数相当于componentDidMount(函数内容)，componentWillUnmount（return的回掉函数）
-    // 第二个参数相当于shouldComponentUpdate（传入的数组内容为变更时 执行一次，当前不需要执行传入空数组）
+    const [date, setDate] = useState(new Date())
+    const [count, setCount] = useState(0)
+    // 第一个参数相当于componentDidMount(函数内容) + componentDidUpdate(函数内容)，componentWillUnmount（return的回掉函数）
+    // 第二个参数传入的数组内容为变更时 执行一次，当前不需要执行传入空数组
     useEffect(
         ()=>{
             const timer = setInterval(()=>{
-                setState(new Date())
+                setDate(new Date())
             },1000)
             return  () => clearInterval(timer)
         },[]
     )
+    useEffect(
+        ()=>{
+            document.title = `You clicked ${count} times`;
+        },[count]
+    )
     return (
         <div>
             <h3>FunctionComponent Page</h3>
+            <button onClick={()=>setCount(count + 1)}>{count}</button>
             <p>{date.toLocaleTimeString()}</p>
         </div>
     )

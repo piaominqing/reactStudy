904f0ea49fc0526d38694be745bf7023b93d3761
LF90187 <piao_minqing@nhn-st.com>
2020-03-01 22:51:14 +0800

    d

--- react-router/src/pages/RouteChildrenPage.js
+++ react-router/src/pages/RouteChildrenPage.js
@@ -1,31 +1,31 @@
 import React, { Component } from "react"; 
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-function ListItemLink({ to, name, ...rest }) {  
-  return (    
-  <Route      
-    path={to}      
-    children={({ match }) => (        
-      <li className={match ? "active" : ""}>          
-        <Link to={to} {...rest}>            
-          {name}          
-        </Link>        
-      </li>      
-    )}    
-    />  
-  ); 
+function ListItemLink({ to, name, ...rest }) {
+  return (
+  <Route
+    path={to}
+    children={({ match }) => (
+      <li className={match ? "active" : ""}>
+        <Link to={to} {...rest}>
+          {name}
+        </Link>
+      </li>
+    )}
+    />
+  );
 }
-export default class RouteChildrenPage extends Component {  
-  render() {    
-    return (      
-      <div>        
-        <h3>RouteChildrenPage</h3> 
-        <Router>          
-          <ul>            
-            <ListItemLink to="/somewhere" name="链接1" />            
-            <ListItemLink to="/somewhere-else" name="链接2" />          
-          </ul>        
-        </Router>      
-      </div>    
-    );  
-  } 
+export default class RouteChildrenPage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>RouteChildrenPage</h3>
+        <Router>
+          <ul>
+            <ListItemLink to="/somewhere" name="链接1" />
+            <ListItemLink to="/somewhere-else" name="链接2" />
+          </ul>
+        </Router>
+      </div>
+    );
+  }
 }

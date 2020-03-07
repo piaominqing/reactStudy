e764e7bf28486958fc1444b3fd7bf4ec9489dd6b
piao-minqing <piao_minqing@nhn-st.com>
2020-03-07 19:01:52 +0800

    add saga

--- react-react_redux/src/pages/ReactReduxPage.js
+++ react-react_redux/src/pages/ReactReduxPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-// import {connect} from 'react-redux'
-import {connect} from '../implements/my-react-redux'
+import {connect} from 'react-redux'
+// import {connect} from '../implements/my-react-redux'
 import {bindActionCreators} from 'redux'
 
 /**
